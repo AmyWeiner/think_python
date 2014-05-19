20463596b187cf825d3b0f35adf1c20a3051afbc
Amy Weiner <arweiner@alumni.stanford.edu>
2014-05-19 11:30:21 -0700

    Complete exercise6.1

---
+++ chapter_06/exercise_6.1.py
@@ -0,0 +1,10 @@
+# The function compare takes integers x and y as parameters, and
+# compares the two values.
+def compare(x, y):
+    if x > y:
+        return 1
+    elif x == y:
+        return 0
+    else:
+        return -1
+
